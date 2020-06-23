2b9a1741dd895ae10c20b89d3c780b790858071f
Vasiliy <Freeneos@icloud.com>
2020-06-23 17:07:05 +0300

    TagListView Swift Package

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TagListViewSPM
 
-A description of this package.
+This is my fork from the [TagListView](https://github.com/ElaWorkshop/TagListView) repository
+
+## Installation
+
+TagListViewSPM is available as a [Swift Package Manager](https://swift.org/package-manager/):
--- Tests/TagListViewSPMTests/TagListViewSPMTests.swift
+++ Tests/TagListViewSPMTests/TagListViewSPMTests.swift
@@ -1,12 +1,13 @@
 import XCTest
 @testable import TagListViewSPM
+import UIKit
 
 final class TagListViewSPMTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TagListViewSPM().text, "Hello, World!")
+        XCTAssertEqual(TagListViewSPM.tagBackgroundColor, UIColor.gray)
     }
 
     static var allTests = [
